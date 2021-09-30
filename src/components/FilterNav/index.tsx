8a9a526a2382ee58388ea983fb4ee585ae48ea32
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-09-30 05:55:36 -0300

    creating Nav for filters

---
+++ src/components/FilterNav/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Container, NavList, NavLink } from './styles';
+
+type  FilterOptionProps = 'all' | 't-shirts' | 'mugs';
+
+interface filterOptionsListProps {
+    title: string,
+    category: FilterOptionProps
+}
+
+const filterOptionsList: filterOptionsListProps[] = [
+    {title: "Todos os Produtos", category: "all"},
+    {title: "Camisetas", category: "t-shirts"},
+    {title: "Canecas", category: "mugs"},
+]
+
+export function FilterNav() {
+    const [filterCategory, setFilterCategory] = useState<FilterOptionProps>('all')
+
+    return (
+        <Container>
+            <NavList>
+                {
+                    filterOptionsList.map(option => {
+                        return (
+                            <NavLink 
+                                key={option.category}
+                                isActive={option.category === filterCategory}
+                                onClick={() => setFilterCategory(option.category)}
+                            >
+                                {option.title}
+                            </NavLink>
+                        )
+                    })
+                }
+            </NavList>
+        </Container>
+    )
+}
---
+++ src/components/FilterNav/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    font-size: 1.6rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media(max-width: 700px) {
+        align-self: flex-start;
+        font-size: 1.3rem;
+    }
+`
+
+export const NavList = styled.ul`
+    display: flex;
+    gap: 3.5rem;
+
+    list-style: none;
+
+    @media(max-width: 700px) {
+        gap: 2rem;
+    }
+`
+
+interface isActiveLink {
+    isActive: boolean;
+}
+
+export const NavLink = styled.li<isActiveLink>`
+    text-transform: uppercase;
+
+    font-weight: ${props => props.isActive ? '600' : '400'};
+    border-bottom: ${props => props.isActive ? '4px solid #ffa585' : 'none'};
+    cursor: pointer;    
+`
