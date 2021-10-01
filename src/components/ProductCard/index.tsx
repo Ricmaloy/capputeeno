6dc7535530294a389e0c50b9b54917b42f76411f
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-10-01 00:54:23 -0300

    chore/create initial product card

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import { formatPrice } from '../../utils/formatPrice';
+import { CardContainer, Details, Title, Divider, Price } from './styles';
+
+
+interface ProductCardProps {
+    id: string,
+    name: string,
+    image_url: string,
+    price_in_cents: number
+}
+
+export function ProductCard({id, name, image_url, price_in_cents}: ProductCardProps) {
+    return (
+        <Link href={`/products/${id}`} passHref>
+            <CardContainer>
+                <Image 
+                    src={image_url}
+                    alt={`Image about the product`}
+                    objectFit="cover"
+                    width={256}
+                    height={300}
+                />
+                <Details>
+                    <Title>{name}</Title>
+                    <Divider />
+                    <Price>{formatPrice(price_in_cents)}</Price>
+                </Details>
+            </CardContainer>
+        </Link>
+    )
+}
---
+++ src/components/ProductCard/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+    background-color: #ffffff;
+    border-radius: 8px 8px 4px 4px;
+    cursor: pointer;
+    overflow: hidden;
+    transition: all 0.3s;
+
+    img {
+        border-radius: 8px 8px 0px 0px;
+        transition: transform 0.3s;
+        transform: scale(1.15);
+    }
+
+    div {
+        width: 100%;
+    }
+
+    &:hover {
+        transform: scale(1.03);
+        box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+
+        img {
+            transform: scale(1);
+        }
+    }
+`
+export const Details = styled.div`
+    padding: 0.8rem 1.2rem;
+`
+
+export const Title = styled.p`
+    font-size: 1.6rem;
+    font-weight: 300;
+    color: #41414d;
+`
+
+export const Divider = styled.div`
+    width: 100%;
+    height: 1px;
+    background-color:  #dce2e6;
+    margin: 0.6rem 0;
+`
+
+export const Price = styled.span`
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: #09090a;
+`
