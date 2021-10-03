bc1205f7296a26f08241947fe10186057fc98fdf
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-10-03 19:26:10 -0300

    chore/creating product page graphql query

---
+++ src/graphql/queries/getProductByID.ts
@@ -0,0 +1,16 @@
+const GET_PRODUCT_BY_ID = /* GraphQL */ `
+    query GET_PRODUCT_BY_ID($id: ID!) {
+        Product(id: $id){
+        id
+        name
+        description
+        category
+        imageUrl: image_url
+        priceInCents: price_in_cents
+        sales
+        createdAt: created_at
+        }
+    }
+`
+
+export default GET_PRODUCT_BY_ID;
