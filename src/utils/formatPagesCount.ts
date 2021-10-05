beda004a046c6e9e003981f983762f4f040527c7
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-10-04 21:30:59 -0300

    chore/create helper funct to format query vars

--- src/utils/formatPagesCount.ts
+++ src/utils/formatPagesCount.ts
@@ -2,5 +2,5 @@ export function formatPagesCount(pages: number): number {
     if(pages === 0)
         return 1;
     else 
-        return Math.floor(pages/12)
+        return Math.ceil(pages/12)
 }
