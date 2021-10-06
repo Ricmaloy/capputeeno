de285b64117ceace9690a3c4a213ef7b9f7b9bb6
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-10-06 13:31:22 -0300

    encha/dynamic infos based on cart context

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from 'next/router';
+import { useCart } from '../../hooks/useCart';
 import { Header } from '../../components/Header';
 import { BackButton } from '../../components/BackButton';
 import { CartItemCard } from '../../components/CartItemCard';
+import { formatPrice } from '../../utils/formatPrice';
 
 import { 
     Container, 
@@ -12,47 +15,23 @@ import {
     CheckoutLink,
     BagList,
     BagPrice,
-    BagItemsList
+    BagItemsList,
+    PriceContainer
 } from '../../styles/pages/Cart';
 
-const Bag = [
-    {
-        "id": "e6fd1296-060c-4122-a4db-f48asdasd238e68131",
-        "name": "Caneca Black Ring",
-        "description": "Et dolore ipsum sapiente et. Sed velit aut mollitia. Tempore ut sapiente dolorem in fugiat voluptas quia. Nemo atque ratione in molestiae quae. Nihil aperiam quia debitis nisi inventore veniam laborum numquam ratione. Labore atque tempora inventore in et.",
-        "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-04.jpg",
-        "price_in_cents": 8020
-    },
-    {
-        "id": "60200d52-e1e9-4aba-9778-7a5asdasdd25edb589",
-        "name": "Caneca preto fosco",
-        "description": "Et dolore ipsum sapiente et. Sed velit aut mollitia. Tempore ut sapiente dolorem in fugiat voluptas quia. Nemo atque ratione in molestiae quae. Nihil aperiam quia debitis nisi inventore veniam laborum numquam ratione. Labore atque tempora inventore in et.",
-        "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-01.jpg",
-        "price_in_cents": 4642
-    },
-    {
-        "id": "3f69242b-ad44-4b48-af9e-46asasdff392b6bbb",
-        "name": "Camiseta not today.",
-        "description": "Et dolore ipsum sapiente et. Sed velit aut mollitia. Tempore ut sapiente dolorem in fugiat voluptas quia. Nemo atque ratione in molestiae quae. Nihil aperiam quia debitis nisi inventore veniam laborum numquam ratione. Labore atque tempora inventore in et.",
-        "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-05.jpg",
-        "price_in_cents": 8904
-    },
-    {
-        "id": "3f69242b-ad44-4b48-af9e-46ff3asdasd92b6bbb",
-        "name": "Camiseta muito fera",
-        "description": "Et dolore ipsum sapiente et. Sed velit aut mollitia. Tempore ut sapiente dolorem in fugiat voluptas quia. Nemo atque ratione in molestiae quae. Nihil aperiam quia debitis nisi inventore veniam laborum numquam ratione. Labore atque tempora inventore in et.",
-        "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-03.jpg",
-        "price_in_cents": 8904
-    }
-]
-
 export default function Cart() {
+    const router = useRouter();
+    const { cart, getCartSize, getCartTotal, handleBuyProducts, getIsFreeFreight } = useCart();
+
+    function handleBuyCart() {
+        handleBuyProducts();
+        router.push('/');
+    }
 
     return (
         <>
             <Header/>
             <Container>
-
                 <BagList>
 
                     <BackButton />
@@ -60,24 +39,32 @@ export default function Cart() {
                     <h1>seu carrinho</h1>
 
                     <BagPrice>
-                        <p>Total (3 produtos) </p>
-                        <span>R$ 161,00</span>
+                        <p>Total ({getCartSize()} produtos) </p>
+                        <span>{formatPrice(getCartTotal())}</span>
                     </BagPrice>
 
                     <BagItemsList>
                         {
-                            Bag.map(item => {
-                                return (
-                                    <CartItemCard
-                                        key={item.id}
-                                        id={item.id}
-                                        description={item.description}
-                                        imageURL={item.image_url}
-                                        name={item.name}
-                                        price={item.price_in_cents}
-                                    />
-                                )
-                            })
+                            cart.length > 0 ? (
+
+                                cart.map(item => {
+                                    return (
+                                        <CartItemCard
+                                            key={item.id}
+                                            id={item.id}
+                                            description={item.description}
+                                            imageURL={item.imageUrl}
+                                            name={item.name}
+                                            price={item.price}
+                                            quantity={item.quantity}
+                                        />
+                                    )
+                                })
+                            ) : (
+                                <>
+                                    <h1>Seu carrinho parece estar vazio! 😔</h1>
+                                </>
+                            )
                         }
                     </BagItemsList>
 
@@ -86,22 +73,39 @@ export default function Cart() {
                 <CheckoutContainer>
                     <CheckoutTitle>Resumo do Pedido</CheckoutTitle>
                     <CheckoutSection>
-                        <span>Subtotoal de produtos</span>
-                        <span>R$ 161,00</span>
+                        <p>Subtotal de produtos</p>
+                        <p>{formatPrice(getCartTotal())}</p>
                     </CheckoutSection>
                     <CheckoutSection>
-                        <span>Entrega</span>
-                        <span>R$ 40,00</span>
+                        <p>Entrega</p>
+                        <PriceContainer isFreefreight={getIsFreeFreight()} >
+                            <p>R$ 40,00</p>
+                            <span>gratuito</span>
+                        </PriceContainer>
                     </CheckoutSection>
 
                     <Divider />
 
                     <CheckoutSection>
                         <h3>Total</h3>
-                        <h3>R$ 201,00</h3>
+                        {
+                            getIsFreeFreight() ? (
+                                <h3>{formatPrice(getCartTotal())}</h3>
+                            ) : (
+                                <h3>{formatPrice(getCartTotal() + 4000)}</h3>
+                            )
+                        }
                     </CheckoutSection>
 
-                    <CheckoutButton>Finalizar a compra</CheckoutButton>
+                    {
+                        cart.length === 0 ? (
+                            <CheckoutButton disabled={true}>Carrinho vazio</CheckoutButton>
+                        ) : (
+
+                            <CheckoutButton onClick={handleBuyCart}>Finalizar a compra</CheckoutButton>
+                        )
+                    }
+
 
                     <CheckoutLink>ajuda</CheckoutLink>
                     <CheckoutLink>reembolsos</CheckoutLink>
