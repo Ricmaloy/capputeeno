0b9da63d7664e0466c3748c2384e552029e18006
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-10-11 14:57:39 -0300

    chore/create get top ten products for page cache

---
+++ src/graphql/queries/getTenTopSalesProducts.ts
@@ -0,0 +1,9 @@
+const GET_TOP_TEN_SALES_PRODUCTS = /* GraphQL */ `
+{
+    allProducts(page: 0, perPage: 10, sortField: "sales", sortOrder: "DESC") {
+      id
+    }
+}
+`
+
+export default GET_TOP_TEN_SALES_PRODUCTS;
