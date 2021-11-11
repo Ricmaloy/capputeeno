7e960382a628ae77dd87b74043d051d775640f42
Ricardo Zamboni <ricardo-testando@hotmail.com>
2021-11-10 23:01:09 -0300

    fix/ set CORS headers

--- src/services/axios/api.ts
+++ src/services/axios/api.ts
@@ -3,3 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL
 })
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] ='https://capputeeno-ricmaloy.vercel.app/';
